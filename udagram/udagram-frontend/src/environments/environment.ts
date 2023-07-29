5ac5f8e1fd9752be6b423439d63c4ac01caafb52
Vinoth <rvinoth2005@gmail.com>
2023-07-29 20:53:28 +0530

    Cloud configurations

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://udagram-api-dev-427860451640.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
